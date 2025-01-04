6c39f04e92e29011a19d16e65cc7ca6c6e9ff269
Ge <geovanasslima@gmail.com>
2025-01-04 02:22:16 -0300

    Creating Base API Files (server and routes)

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+router.get('/test', (req: Request, res: Response) => {
+  res.json({ ok: true });
+  return;
+});
+
+export { router };
