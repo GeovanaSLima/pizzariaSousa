67a11949b515fc45984e4bb5ac935ab95e440ca5
Ge <geovanasslima@gmail.com>
2025-01-09 22:08:24 -0300

    Creating login page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,39 @@
-import styles from './page.module.scss'
+import Link from 'next/link';
+import styles from './page.module.scss';
+import logoImg from '/public/logo.svg';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main>
-      <h1 className={styles.teste}>Test</h1>
-    </main>
+    <>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo Pizzaria Sousa" width={500} />
+
+        <section className={styles.login}>
+          <form>
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder="Digite seu e-mail..."
+              className={styles.input}
+            />
+            <input
+              type="password"
+              required
+              name="password"
+              placeholder="**************"
+              className={styles.input}
+            />
+
+            <button type="submit">Acessar</button>
+          </form>
+
+          <Link href="/register" className={styles.registerText}>
+            Não possui uma conta? Cadastre-se
+          </Link>
+        </section>
+      </div>
+    </>
   );
 }
