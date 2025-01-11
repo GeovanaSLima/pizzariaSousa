67ba2289860176b66675719f9af142b9a9fcdbd7
Ge <geovanasslima@gmail.com>
2025-01-11 19:41:30 -0300

    Consuming API to create products on database

--- frontend/src/app/dashboard/product/page.tsx
+++ frontend/src/app/dashboard/product/page.tsx
@@ -1,12 +1,15 @@
 import styles from '@/app/page.module.scss';
 import { Form } from '../_components/form';
+import { getCookieServer } from '@/lib/cookieServer';
+import { api } from '@/services/api';
 
-export default function Product() {
-  return (
-    <main className={styles.categoryContainer}>
-      <h1>Novo Produto</h1>
+export default async function Product() {
+  const token = await getCookieServer();
+  const response = await api.get('/category', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
-      <Form />
-    </main>
-  );
+  return <Form categories={response.data} />;
 }
