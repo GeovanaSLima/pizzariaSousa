a01a51606fb93544252074b6da6e83df41b52929
Ge <geovanasslima@gmail.com>
2025-01-16 01:16:35 -0300

    fix dist

--- backend/dist/main/server.js
+++ backend/dist/main/server.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-require("tsconfig-paths/register");
 const app_1 = require("./app");
 const config_1 = require("./config/config");
 const app = new app_1.App();
